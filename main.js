e9bbc89098d51f7b4c7594347c8e3ed37cd316f8
Akash2746 <60676443+Akash2746@users.noreply.github.com>
2021-05-03 12:33:07 -0400

    Final commit
    read input from command line and store these string values in an array. convert each character into words only if the values in string array is an integer.

--- main.js
+++ main.js
@@ -14,9 +14,8 @@ map[9]='Nine';
 
 //readind inputs from command line and 
 //string 's' to store converted digits to words.
-//let string=process.argv.slice(2);
+let string=process.argv.slice(2);
 let  s='';
-let string=['10','300','5];
     
 //loops to get each digit in the input array. 
 string.forEach(element => {
