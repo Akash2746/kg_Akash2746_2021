401196aa94067901c610b7adc61986439b8c4125
Akash2746 <60676443+Akash2746@users.noreply.github.com>
2021-05-03 12:24:22 -0400

    read inputs from a string array
    numbers in string array is converted to words

--- main.js
+++ main.js
@@ -14,9 +14,10 @@ map[9]='Nine';
 
 //readind inputs from command line and 
 //string 's' to store converted digits to words.
-let string=process.argv.slice(2);
+//let string=process.argv.slice(2);
 let  s='';
-
+let string=['10','300','5];
+    
 //loops to get each digit in the input array. 
 string.forEach(element => {
     for(let i=0;i<element.length;i++){
